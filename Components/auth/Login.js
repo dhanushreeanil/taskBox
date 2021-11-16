3e00cb6cb22c361b13a20e9c600f159155f3ddd9
Dhanushree <dhanushreeanil@gmail.com>
2021-11-16 21:38:11 +0530

    changed register and login components with formik and yup

--- Components/auth/Login.js
+++ Components/auth/Login.js
@@ -1,69 +1,142 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 import { startLoginData } from '../../actions/usersAction'
 
 const Login = (props) => {
 
-    const [loginUser, setLoginUser] = useState({
-        email : "",
-        password : ""
-    })
-
     const dispatch = useDispatch()
 
-    const handleChange = (e) => {
-        if(e.target.name === "email"){
-            setLoginUser({...loginUser , email : e.target.value })
-        }
-        else if(e.target.name === "password"){
-            setLoginUser({...loginUser, password : e.target.value })
-        }
-    }
     const resetForm = () =>{
-        setLoginUser({})
+        return initialValues
+        // props.form.values({})
     }
+
     const redirect = () =>{
         props.history.push("/")
     }
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        const { email, password } = loginUser
-        const formData = {
-            email,
-            password
-        }
-        console.log("login-data", formData)
-        dispatch(startLoginData(formData,resetForm,redirect))
+
+    const initialValues = {
+        email : "",
+        password : ""
+    }
+
+    const onSubmit = values =>{
+        dispatch(startLoginData(values, resetForm, redirect))
+        console.log("formdata-values", values)
         alert(`successfully Logged-In`)
         props.handleAuth()
     }
 
+    const validationSchema = Yup.object({
+        email : Yup.string().email("Invalid email format").required("Required*"),
+        password : Yup.string().required("Required*")
+    })
+
     return (
         <div className="container-fluid">
             <div className="jumbotron">
-                <h2>Login to your Account</h2>
+                <h2> Login With Us</h2>
             </div>
-            <form className="form-group" onSubmit={ handleSubmit }>
-                <input class="form-control"
+            <Formik initialValues = { initialValues }
+                validationSchema = { validationSchema }
+                onSubmit = { onSubmit } 
+            >
+            <Form>
+                {/* automatically links handleSubmit event to method passed into formik */}
+                <Field className="form-control"
                     type="text" 
+                    name="email"
                     placeholder="Enter email" 
-                    value={ loginUser.email }
-                    onChange = { handleChange }
-                    name="email"    
-                /><br/>
-                <input class="form-control"
+                />
+                <ErrorMessage name="email" />
+                <br/>
+                <Field className="form-control"
                     type="password" 
+                    name="password" 
                     placeholder="Enter password" 
-                    value={ loginUser.password }
-                    onChange = { handleChange } 
-                    name="password"
-                /><br/>
-                <input class="btn btn-info" type="submit" value="Login" />
-                <input class="btn btn-danger" type="submit" value="Cancel" />
-            </form>
+                />
+                <ErrorMessage name="password" /> 
+                <br/>
+                <Field  className="btn btn-info" type="submit" value="Login" /> 
+                <Field  className="btn btn-danger" type="submit" value="Cancel" />
+            </Form>
+            </Formik>
         </div>
     )
 }
 
 export default Login
+
+
+// import React, { useState } from 'react'
+// import { useDispatch } from 'react-redux'
+
+// import { startLoginData } from '../../actions/usersAction'
+
+// const Login = (props) => {
+
+//     const [loginUser, setLoginUser] = useState({
+//         email : "",
+//         password : ""
+//     })
+
+//     const dispatch = useDispatch()
+
+//     // const handleChange = (e) => {
+//     //     if(e.target.name === "email"){
+//     //         setLoginUser({...loginUser , email : e.target.value })
+//     //     }
+//     //     else if(e.target.name === "password"){
+//     //         setLoginUser({...loginUser, password : e.target.value })
+//     //     }
+//     // }
+//     const resetForm = () =>{
+//         setLoginUser({})
+//     }
+//     const redirect = () =>{
+//         props.history.push("/")
+//     }
+//     // const handleSubmit = (e) =>{
+//     //     e.preventDefault()
+//     //     const { email, password } = loginUser
+//     //     const formData = {
+//     //         email,
+//     //         password
+//     //     }
+//     //     console.log("login-data", formData)
+//     //     dispatch(startLoginData(formData,resetForm,redirect))
+//     //     alert(`successfully Logged-In`)
+//     //     props.handleAuth()
+//     // }
+
+//     return (
+//         <div className="container-fluid">
+//             <div className="jumbotron">
+//                 <h2>Login to your Account</h2>
+//             </div>
+//             <form className="form-group" onSubmit={ handleSubmit }>
+//                 <input class="form-control"
+//                     type="text" 
+//                     placeholder="Enter email" 
+//                     value={ loginUser.email }
+//                     onChange = { handleChange }
+//                     name="email"    
+//                 /><br/>
+//                 <input class="form-control"
+//                     type="password" 
+//                     placeholder="Enter password" 
+//                     value={ loginUser.password }
+//                     onChange = { handleChange } 
+//                     name="password"
+//                 /><br/>
+//                 <input class="btn btn-info" type="submit" value="Login" />
+//                 <input class="btn btn-danger" type="submit" value="Cancel" />
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default Login
--- Components/auth/Register.js
+++ Components/auth/Register.js
@@ -1,83 +1,166 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 import { startRegisterData } from '../../actions/usersAction'
 
 const Register = (props) => {
 
-    const [registerUser, setRegisterUser] = useState({
-        username : "",
-        email : "",
-        password : ""
-    })
-
     const dispatch = useDispatch()
 
-    const handleChange = (e) => {
-
-        if(e.target.name === "username"){
-            setRegisterUser({...registerUser, username: e.target.value})
-        }
-        else if(e.target.name === "email"){
-            setRegisterUser({...registerUser, email : e.target.value})
-        }
-        else if(e.target.name === "password"){
-            setRegisterUser({...registerUser, password:e.target.value})
-        }
-        console.log("user", registerUser)
-    }
-
     const resetForm = () =>{
-        setRegisterUser({})
+        return initialValues
+        // props.form.values({})
     }
+
+    console.log()
+
     const redirect = () =>{
         props.history.push("/login")
     }
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        const { username, email, password } = registerUser
-        const formData = {
-            username,
-            email,
-            password
-        }
-        dispatch(startRegisterData(formData, resetForm, redirect))
+
+    const initialValues = {
+        username : "",
+        email : "dhanu",
+        password : ""
     }
+
+    const onSubmit = values =>{
+        dispatch(startRegisterData(values, resetForm, redirect))
+        console.log("formdata-values", values)
+    }
+
+    const validationSchema = Yup.object({
+        username : Yup.string().required("Required*"),
+        email : Yup.string().email("Invalid email format").required("Required*"),
+        password : Yup.string().required("Required*")
+    })
 
     return (
         <div className="container-fluid">
             <div className="jumbotron">
                 <h2> Register With Us</h2>
             </div>
-            
-            <form className="form-group" onSubmit={ handleSubmit }>
-                <input class="form-control"
+            <Formik initialValues = { initialValues }
+                validationSchema = { validationSchema }
+                onSubmit = { onSubmit } 
+            >
+            <Form>
+                {/* automatically links handleSubmit event to method passed into formik */}
+                <Field className="form-control"
                     type="text" 
-                    placeholder="Enter username" 
-                    value={ registerUser.username }
-                    onChange = { handleChange }  
                     name="username"  
-                /><br/>
-                <input class="form-control"
+                    placeholder="Enter username" 
+                />
+                <ErrorMessage name="username" />
+                <br/>
+                <Field className="form-control"
                     type="text" 
-                    placeholder="Enter email" 
-                    value={ registerUser.email }
-                    onChange = { handleChange }    
                     name="email"
-                /><br/>
-                <input class="form-control"
+                    placeholder="Enter email" 
+                />
+                <ErrorMessage name="email" />
+                <br/>
+                <Field className="form-control"
                     type="password" 
-                    placeholder="Enter password" 
-                    value={ registerUser.password }
-                    onChange = { handleChange }
                     name="password" 
-                /><br/>
-                <input  class="btn btn-info" type="submit" value="Register" /> 
-                <input  class="btn btn-danger" type="submit" value="Cancel" />
-            </form>
+                    placeholder="Enter password" 
+                />
+                <ErrorMessage name="password" /> 
+                <br/>
+                <Field  className="btn btn-info" type="submit" value="Register" /> 
+                <Field  className="btn btn-danger" type="submit" value="Cancel" />
+            </Form>
+            </Formik>
         </div>
     )
 }
 
 export default Register
 
+
+// using redux
+
+// import React, { useState } from 'react'
+// import { useDispatch } from 'react-redux'
+
+// import { startRegisterData } from '../../actions/usersAction'
+
+// const Register = (props) => {
+
+//     const [registerUser, setRegisterUser] = useState({
+//         username : "",
+//         email : "",
+//         password : ""
+//     })
+
+//     const dispatch = useDispatch()
+
+//     const handleChange = (e) => {
+
+//         if(e.target.name === "username"){
+//             setRegisterUser({...registerUser, username: e.target.value})
+//         }
+//         else if(e.target.name === "email"){
+//             setRegisterUser({...registerUser, email : e.target.value})
+//         }
+//         else if(e.target.name === "password"){
+//             setRegisterUser({...registerUser, password:e.target.value})
+//         }
+//         console.log("user", registerUser)
+//     }
+
+//     const resetForm = () =>{
+//         setRegisterUser({})
+//     }
+//     const redirect = () =>{
+//         props.history.push("/login")
+//     }
+//     const handleSubmit = (e) =>{
+//         e.preventDefault()
+//         const { username, email, password } = registerUser
+//         const formData = {
+//             username,
+//             email,
+//             password
+//         }
+//         dispatch(startRegisterData(formData, resetForm, redirect))
+//     }
+
+//     return (
+//         <div className="container-fluid">
+//             <div className="jumbotron">
+//                 <h2> Register With Us</h2>
+//             </div>
+            
+//             <form className="form-group" onSubmit={ handleSubmit }>
+//                 <Field class="form-control"
+//                     type="text" 
+//                     placeholder="Enter username" 
+//                     value={ registerUser.username }
+//                     onChange = { handleChange }  
+//                     name="username"  
+//                 /><br/>
+//                 <Field class="form-control"
+//                     type="text" 
+//                     placeholder="Enter email" 
+//                     value={ registerUser.email }
+//                     onChange = { handleChange }    
+//                     name="email"
+//                 /><br/>
+//                 <Field class="form-control"
+//                     type="password" 
+//                     placeholder="Enter password" 
+//                     value={ registerUser.password }
+//                     onChange = { handleChange }
+//                     name="password" 
+//                 /><br/>
+//                 <Field  class="btn btn-info" type="submit" value="Register" /> 
+//                 <Field  class="btn btn-danger" type="submit" value="Cancel" />
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default Register
